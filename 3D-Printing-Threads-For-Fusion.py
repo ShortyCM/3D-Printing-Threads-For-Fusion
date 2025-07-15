73ab420c9e8437c5bdb08bdc8e186d02033f48f7
ShortyCM <39855412+ShortyCM@users.noreply.github.com>
2025-07-15 10:15:05 -0700

    Add files via upload

---
+++ 3D-Printing-Threads-For-Fusion.py
@@ -0,0 +1,150 @@
+# This file is part of 3D-Printing-Threads-For-Fusion.
+#
+# [Project Name] is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# [Project Name] is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with [Project Name].  If not, see <https://www.gnu.org/licenses/>.
+
+import xml.etree.ElementTree as ET
+import os
+import glob
+import shutil
+import sys
+
+# Define the linear adjustment function with a ceiling
+def calculate_adjustment(pitch_mm):
+    adjustment = pitch_mm * 0.16  # Coefficient for linear scaling
+    return min(adjustment, 0.3)  # Apply a ceiling of 0.3 mm
+
+def adjust_diameter(diameter, adjustment, unit):
+    try:
+        diameter_value = float(diameter)
+        if unit == 'in':
+            adjustment /= 25.4  # Convert mm adjustment to inches
+        return diameter_value + adjustment
+    except ValueError:
+        return diameter
+
+def process_thread(thread, pitch, unit, is_internal):
+    pitch_mm = pitch if unit == 'mm' else 25.4 / pitch
+    adjustment = calculate_adjustment(pitch_mm)
+    if is_internal:
+        adjustment = abs(adjustment)  # Increase for internal threads
+    else:
+        adjustment = -abs(adjustment)  # Decrease for external threads
+
+    major_dia = thread.find('MajorDia')
+    if major_dia is not None:
+        major_dia.text = f"{adjust_diameter(major_dia.text, adjustment, unit):.4f}"
+    
+    pitch_dia = thread.find('PitchDia')
+    if pitch_dia is not None:
+        pitch_dia.text = f"{adjust_diameter(pitch_dia.text, adjustment, unit):.4f}"
+    
+    minor_dia = thread.find('MinorDia')
+    if minor_dia is not None:
+        minor_dia.text = f"{adjust_diameter(minor_dia.text, adjustment, unit):.4f}"
+
+    if is_internal:
+        tap_drill = thread.find('TapDrill')
+        if tap_drill is not None and tap_drill.text:
+            try:
+                tap_drill.text = f"{adjust_diameter(tap_drill.text, adjustment, unit):.4f}"
+            except ValueError:
+                pass
+
+def process_designation(designation, unit):
+    pitch = None
+    tpi = designation.find('TPI')
+    if tpi is not None:
+        pitch = float(tpi.text)
+    else:
+        pitch = float(designation.find('Pitch').text)
+    
+    for thread in designation.findall('Thread'):
+        gender = thread.find('Gender').text
+        is_internal = gender == 'internal'
+        process_thread(thread, pitch, unit, is_internal)
+
+def process_thread_size(thread_size, unit):
+    for designation in thread_size.findall('Designation'):
+        process_designation(designation, unit)
+
+def process_thread_type(thread_type):
+    unit = thread_type.find('Unit').text  # Determine the unit of measurement
+    
+    name = thread_type.find('Name')
+    if name is not None:
+        name.text += " for 3D printing"
+    
+    custom_name = thread_type.find('CustomName')
+    if custom_name is not None:
+        custom_name.text += " for 3D printing"
+
+    for thread_size in thread_type.findall('ThreadSize'):
+        process_thread_size(thread_size, unit)
+
+def adjust_thread_definitions(input_file, output_file):
+    tree = ET.parse(input_file)
+    root = tree.getroot()
+
+    for thread_type in root.findall('./ThreadSize/..'):
+        process_thread_type(thread_type)
+
+    tree.write(output_file, encoding='UTF-8', xml_declaration=True)
+
+def find_latest_thread_data_directory():
+    base_dir = os.path.join(os.getenv('LOCALAPPDATA'), 'Autodesk', 'webdeploy', 'production')
+    latest_subdir = None
+    latest_time = None
+    for subdir in os.listdir(base_dir):
+        candidate = os.path.join(base_dir, subdir, 'Fusion', 'Server', 'Fusion', 'Configuration', 'ThreadData')
+        if os.path.isdir(candidate):
+            creation_time = os.path.getctime(candidate)
+            if latest_time is None or creation_time > latest_time:
+                latest_time = creation_time
+                latest_subdir = candidate
+    return latest_subdir
+
+def copy_custom_files(target_dir):
+    if getattr(sys, 'frozen', False):
+        # Running in a PyInstaller bundle
+        script_dir = os.path.dirname(sys.executable)
+    else:
+        # Running in a normal Python environment
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    for file in glob.glob(os.path.join(script_dir, "*.xml")):
+        shutil.copy(file, target_dir)
+
+def main():
+    thread_data_dir = find_latest_thread_data_directory()
+    if thread_data_dir is None:
+        print("ThreadData directory not found.")
+        return
+
+    # Copy any custom XML files to the target directory
+    copy_custom_files(thread_data_dir)
+    
+    # Delete any existing -3Dprinting.xml files
+    for file in glob.glob(os.path.join(thread_data_dir, "*-3Dprinting.xml")):
+        os.remove(file)
+
+    # Process each XML file and write the adjusted content to a new file
+    for file in glob.glob(os.path.join(thread_data_dir, "*.xml")):
+        if "-3Dprinting" not in file:
+            base_name = os.path.basename(file)
+            base_name_without_ext = os.path.splitext(base_name)[0]
+            output_file = os.path.join(thread_data_dir, base_name_without_ext + "-3Dprinting.xml")
+            adjust_thread_definitions(file, output_file)
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# 3D-Printing-Threads-For-Fusion
-Automatically adjusts Fusion's threads definitions to work with FDM 3D printing
+While testing this I first tried just using a single offset for all the threads, but that didn't work very well at all. Smaller
+thread pitches don't seem to need as much compensation as larger pitches do. And at a certain point there also doesn't seem to
+be any need to continue using more and more compensation. So I've made it use smaller offsets for smaller thread pitches, and as
+the thread pitches get larger the compensation tops out at 0.2 mm, as I haven't found that I've needed any more than that. This is
+probably related to both the layer heights and the nozzle diameter, of which I usually use 0.2 mm layer height and a 0.4 mm nozzle,
+as I'm sure is popular. So I think for most people this will work as-is, but if you're using a different nozzle and/or layer
+heights then you might want to tweak things a bit. The coefficient and the ceiling are the first things listed, so you don't have
+to go hunting for them if you are going to try playing with them.
+
+So far, this seems to work at least down to 0.5 mm thread pitches, and I've tried up to as large as 8 TPI (3.175 mm), and things
+seem to be alright. Even down to 0.5 mm with a 0.2 mm layer height isn't too bad, but you'll obviously get smoother threads if
+you're also using smaller/variable layer heights once you start going to smaller threads like that. Naturally, you'll probably get
+better results with 3D printing if you're using threads a little larger than 0.5 mm, though. Hehe. But they do work. I haven't
+tried any smaller than that.
+
+Anyway, here's the code. If you don't have python installed, and for whatever reason don't want to install it, I could make up a
+one-file exe with pyinstaller, I suppose, but I figured most of the 3D printing crowd probably wouldn't mind installing python, if
+they don't already have it installed anyway.
+
+**2024-06-12 update: I have updated the script to do two things. First, it now checks the creation dates of the directories to
+ensure it is actually working on the latest directory. That way when Fusion 360 updates come out it will actually find the right
+directory to work with. Second, it will now also copy any custom threads xml files that you keep in the same directory as the
+script to the latest working directory. It does this before the 3D printing tweaks, so that your custom threads files will also get
+3D printing versions.
+
+**2024-07-13 update: I've upped the ceiling from 0.2 mm to 0.3 mm because of an experience I had over the last week. I did a job
+for a buddy of mine that required some M80x6 threads, and there seemed to be a problem with such a large thread pitch. It was
+pretty tight, so I decided to change the ceiling from 0.2 mm to 0.4 mm and try the parts again. That time they fit, but I felt the
+fit was a bit looser than necessary, so I've updated the script now to try using a 0.3 mm script. I didn't print those parts again,
+because even though it was a little more wiggle than I'd prefer, they went together nicely and worked as intended. But since the
+original 0.2 mm ceiling resulted in parts that were a little too tight I've bumped it to 0.3 mm. Naturally, this will only make a
+difference on thread pitches that are larger than 1.25 mm, as the offsets will be the same at that pitch and below. This means
+thread pitches between 1.25 mm and 1.875 mm will now scale from between 0.2 mm and 0.3 mm offsets, and above 1.875 mm will use that
+0.3 mm ceiling now.
+
+**2024-07-27 update: Threads tested so far:
+
+M2.5x0.45
+
+M3x0.5
+
+M4.5x0.5
+
+M15x1.5
+
+M18x2.5
+
+M23x0.6
+
+M25x2
+
+M35x1.5
+
+M80x6
+
+5-40 UNC (#5, 0.125")
+
+1/4-20 UNC
+
+1/4-28 UNF
+
+5/16-18 UNC
+
+3/8-16 UNC
+
+1/2-28 UNEF
+
+5/8-28 UN
+
+3/4-20 UNEF
+
+3/4-32 UN
+
+13/16-32 UN
+
+1-20 UNEF (1")
+
+1-40 UN (1")
